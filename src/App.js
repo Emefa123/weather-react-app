aacc531566ed128c116679f7651c34bedc1bde8b
Emefa123 <margaretemepha@gmail.com>
2023-11-21 18:58:41 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="" target="_blank">
-          Margaret
-        </a>{" "}
-        and is{" "}
-        <a href="" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="www.linkedin.com/in/margaret-kudzordzinawo-37956b283"
+            target="_blank"
+          >
+            Margaret
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Emefa123/weather-react-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
