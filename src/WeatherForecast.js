be14d756238f2f426b0c083ca9215053b4eb5640
Emefa123 <margaretemepha@gmail.com>
2023-11-24 11:21:06 +0000

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
