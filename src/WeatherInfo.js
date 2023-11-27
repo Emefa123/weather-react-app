a2ab74c54e2feb4d187bf2e32aab9423df347a94
Emefa123 <margaretemepha@gmail.com>
2023-11-27 11:31:37 +0000

    Update Weather.css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,12 +7,23 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
+      <div className="row mt-2">
+        <div className="col-6">
+          <ul>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind}km/h</li>
+          </ul>
+        </div>
+      </div>
+
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex">
@@ -24,12 +35,6 @@ export default function WeatherInfo(props) {
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind}km/h</li>
-          </ul>
         </div>
       </div>
     </div>
